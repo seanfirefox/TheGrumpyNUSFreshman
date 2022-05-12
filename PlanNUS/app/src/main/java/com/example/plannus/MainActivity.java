67d01f494186333e6d525d5b6626ebd201c81ec5
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-05-12 15:34:51 +0800

    Add README

---
+++ PlanNUS/app/src/main/java/com/example/plannus/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.plannus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+        }
+    }
+}

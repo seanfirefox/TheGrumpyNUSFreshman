926536dca7cb612ae67e835acb1dd50e6bbe7220
Tan Li Thai Sean <seanfirefox@gmail.com>
2022-05-16 11:18:15 +0800

    Structure of ToDoList and xml fixes

---
+++ PlanNUS/app/src/main/java/com/example/plannus/ToDoList.java
@@ -0,0 +1,14 @@
+package com.example.plannus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToDoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do_list);
+    }
+}

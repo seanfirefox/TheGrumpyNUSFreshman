27164994ad7a4dcccf5c7da2099a1183c8c22513
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-05-16 22:56:46 +0800

    Migrate from FireBase Auth to FireStore for Task List

--- PlanNUS/app/src/main/java/com/example/plannus/AddTaskActivity.java
+++ PlanNUS/app/src/main/java/com/example/plannus/AddTaskActivity.java
@@ -1,16 +1,25 @@
 package com.example.plannus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class AddTaskActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,14 +27,18 @@ public class AddTaskActivity extends AppCompatActivity implements View.OnClickLi
     private Button saveTask;
     private ImageButton backButton;
     private EditText newTask, newStatus, newTag, dueDate, dueTime, plannedDate, plannedTime;
-    private FirebaseDatabase dataBase;
+    private FirebaseFirestore fDataBase;
+    private FirebaseAuth fAuth;
+    private String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
-        dataBase = FirebaseDatabase.getInstance("https://plannus-cad5f-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        fAuth = FirebaseAuth.getInstance();
+        fDataBase = FirebaseFirestore.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
 
         saveTask = findViewById(R.id.saveButton);
         saveTask.setOnClickListener(this);
@@ -60,11 +73,18 @@ public class AddTaskActivity extends AppCompatActivity implements View.OnClickLi
         String planDate = plannedDate.getText().toString().trim();
         String planTime = plannedTime.getText().toString().trim();
 
-        DatabaseReference ref = dataBase.getReference("Users");
         ToDoTask t = new ToDoTask(tag, task, stats, deadlineDate);
-        ref.child("Users").child(task).setValue(t);
-
+        DocumentReference docRef = fDataBase.collection("Users").document(userID);
+        Map<String, ToDoTask> taskList = new HashMap<>();
+        taskList.put(task, t);
+        docRef.set(taskList, SetOptions.merge()).addOnSuccessListener((OnSuccessListener<? super Void>) (aVoid) -> {
+            Log.d("TaskCreated", "onSuccess: Task is created");
+        } ).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("TaskFail", "onFailure: "+ e.toString());
+            }
+        });
     }
-
 }
 

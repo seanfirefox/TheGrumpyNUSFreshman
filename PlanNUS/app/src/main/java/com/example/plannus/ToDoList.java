e07f79928932eca677c62e3e5a844c67659cf60a
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-05-20 23:50:12 +0800

    Migrate to Firestore UI

--- PlanNUS/app/src/main/java/com/example/plannus/ToDoList.java
+++ PlanNUS/app/src/main/java/com/example/plannus/ToDoList.java
@@ -12,7 +12,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -29,88 +31,102 @@ public class ToDoList extends AppCompatActivity implements View.OnClickListener 
     private ToDoListAdapter toDoListAdapter;
     private ArrayList<ToDoTask> list;
     private Button createTask;
-    private FirebaseAuth mAuth;
-    private String userID;
     private ProgressDialog progressDialog;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private String userID;
+    private CollectionReference taskRef;
+    private ToDoListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
 
-        recyclerView = findViewById(R.id.taskListAnnouncements);
-        fireStore = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
         userID = mAuth.getCurrentUser().getUid();
+        taskRef = db.collection("Users")
+                .document(this.userID)
+                .collection("Tasks");
 
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        setUpRecyclerView();
 
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setCancelable(false);
-        progressDialog.setMessage("Fetching Data...");
-        progressDialog.show();
-
-        createTask = findViewById(R.id.createTask);
-        createTask.setOnClickListener(this);
-        list = new ArrayList<ToDoTask>();
-        toDoListAdapter = new ToDoListAdapter(ToDoList.this, list);
-        recyclerView.setAdapter(toDoListAdapter);
-
-        EventChangeListener();
+//        recyclerView = findViewById(R.id.taskListAnnouncements);
+//        fireStore = FirebaseFirestore.getInstance();
+//        mAuth = FirebaseAuth.getInstance();
+//        userID = mAuth.getCurrentUser().getUid();
+//
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        progressDialog = new ProgressDialog(this);
+//        progressDialog.setCancelable(false);
+//        progressDialog.setMessage("Fetching Data...");
+//        progressDialog.show();
+//
+//        createTask = findViewById(R.id.createTask);
+//        createTask.setOnClickListener(this);
+//        list = new ArrayList<ToDoTask>();
+//        toDoListAdapter = new ToDoListAdapter(ToDoList.this, list);
+//        recyclerView.setAdapter(toDoListAdapter);
+//
+//        EventChangeListener();
     }
 
-    private void EventChangeListener() {
-        fireStore.collection("Users")
-                .document(this.userID)
-                .collection("Tasks")
-                .orderBy("deadLineTime", Query.Direction.ASCENDING)
-                .addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                        if (error != null) {
-                            if(progressDialog.isShowing()) {
-                                progressDialog.dismiss();
-                            }
-                            Log.e("Firestore error", error.getMessage());
-                            return;
-                        }
-                        for (DocumentChange dc : value.getDocumentChanges()) {
-                            if(dc.getType() == DocumentChange.Type.ADDED) {
-                                list.add(dc.getDocument().toObject(ToDoTask.class));
-                            }
-                        }
-
-                        toDoListAdapter.notifyDataSetChanged();
-                        if(progressDialog.isShowing()) {
-                            progressDialog.dismiss();
-                        }
-                    }
-                });
-//                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//    private void EventChangeListener() {
+//        fireStore.collection("Users")
+//                .document(this.userID)
+//                .collection("Tasks")
+//                .orderBy("deadLineTime", Query.Direction.ASCENDING)
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
 //                    @Override
-//                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            DocumentSnapshot documentSnapshot = task.getResult();
-//                            if (progressDialog.isShowing()) {
+//                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                        if (error != null) {
+//                            if(progressDialog.isShowing()) {
 //                                progressDialog.dismiss();
 //                            }
-//                            if (documentSnapshot.exists()) {
-//                                Log.d("Successful", "DocumentSnapshot data: " + documentSnapshot.getData());
-//                                list.add(documentSnapshot.toObject(ToDoTask.class));
-//                            } else {
-//                                Log.d("Fail", "No Such Document");
+//                            Log.e("Firestore error", error.getMessage());
+//                            return;
+//                        }
+//                        for (DocumentChange dc : value.getDocumentChanges()) {
+//                            if(dc.getType() == DocumentChange.Type.ADDED) {
+//                                list.add(dc.getDocument().toObject(ToDoTask.class));
 //                            }
-//                        } else {
-//                            if (progressDialog.isShowing()) {
-//                                progressDialog.dismiss();
-//                            }
-//                            Log.d("Fail", "get failed with", task.getException());
+//                        }
+//
+//                        toDoListAdapter.notifyDataSetChanged();
+//                        if(progressDialog.isShowing()) {
+//                            progressDialog.dismiss();
 //                        }
 //                    }
 //                });
+//    }
 
+    private void setUpRecyclerView() {
+        Query query = taskRef.orderBy("deadLineDate", Query.Direction.ASCENDING)
+                .orderBy("deadLineTime", Query.Direction.ASCENDING);
 
+        FirestoreRecyclerOptions<ToDoTask> options = new FirestoreRecyclerOptions.Builder<ToDoTask>()
+                .setQuery(query, ToDoTask.class)
+                .build();
+        adapter = new ToDoListAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.taskListAnnouncements);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     @Override
--- PlanNUS/app/src/main/java/com/example/plannus/ToDoListAdapter.java
+++ PlanNUS/app/src/main/java/com/example/plannus/ToDoListAdapter.java
@@ -9,57 +9,107 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
 import java.util.ArrayList;
 
-public class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.MyViewHolder> {
+public class ToDoListAdapter extends FirestoreRecyclerAdapter<ToDoTask, ToDoListAdapter.TaskHolder> {
 
-    Context context;
+    public ToDoListAdapter(@NonNull FirestoreRecyclerOptions<ToDoTask> options) {
+        super(options);
+    }
 
-    ArrayList<ToDoTask> list;
-
-    public ToDoListAdapter(Context context, ArrayList<ToDoTask> list) {
-        this.context = context;
-        this.list = list;
+    @Override
+    protected void onBindViewHolder(@NonNull TaskHolder holder, int position, @NonNull ToDoTask model) {
+        holder.taskName.setText(model.getTask());
+        holder.tagName.setText(model.getModuleName());
+        holder.status.setText(model.getStatus());
+        holder.dueDate.setText(model.getDeadLineDate());
+        holder.dueTime.setText(model.getDeadLineTime());
+        holder.plannedDate.setText(model.getPlannedDate());
+        holder.plannedTime.setText(model.getPlannedTime());
     }
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.todotask, parent, false);
-        return new MyViewHolder(v);
+    public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.todotask, parent, false);
+        return new TaskHolder(v);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        ToDoTask toDoTask = list.get(position);
-        holder.moduleName.setText(toDoTask.getModuleName());
-        holder.task.setText(toDoTask.getTask());
-        holder.status.setText(toDoTask.getStatus() + "%");
-        holder.deadlineDate.setText(toDoTask.getDeadLineDate());
-        holder.deadlineTime.setText(toDoTask.getDeadLineTime());
-        holder.plannedDate.setText(toDoTask.getPlannedDate());
-        holder.plannedTime.setText(toDoTask.getPlannedTime());
-    }
+    class TaskHolder extends RecyclerView.ViewHolder {
+        TextView taskName;
+        TextView tagName;
+        TextView status;
+        TextView dueDate;
+        TextView dueTime;
+        TextView plannedDate;
+        TextView plannedTime;
 
-    @Override
-    public int getItemCount() {
-        return list.size();
-    }
-
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-
-        TextView moduleName, task, status, deadlineDate, deadlineTime, plannedDate, plannedTime;
-
-        public MyViewHolder(@NonNull View itemView) {
+        public TaskHolder(View itemView) {
             super(itemView);
-
-            moduleName = itemView.findViewById(R.id.moduleNameAnnouncements);
-            task = itemView.findViewById(R.id.toDoTaskAnnouncements);
-            status = itemView.findViewById(R.id.status);
-            deadlineDate = itemView.findViewById(R.id.deadlineDateAnnouncements);
-            deadlineTime = itemView.findViewById(R.id.deadlineTimeAnnouncements);
-            plannedDate = itemView.findViewById(R.id.plannedDate);
-            plannedTime = itemView.findViewById(R.id.plannedTime);
+            taskName = itemView.findViewById(R.id.toDoTaskName);
+            tagName = itemView.findViewById(R.id.tagName);
+            status = itemView.findViewById(R.id.statusInt);
+            dueDate = itemView.findViewById(R.id.deadlineDate);
+            dueTime = itemView.findViewById(R.id.deadlineTime);
+            plannedDate = itemView.findViewById(R.id.PlannedDate);
+            plannedTime = itemView.findViewById(R.id.PlannedTime);
         }
     }
 }
+
+//public class ToDoListAdapter extends RecyclerView.Adapter<ToDoListAdapter.MyViewHolder> {
+//
+//    Context context;
+//
+//    ArrayList<ToDoTask> list;
+//
+//    public ToDoListAdapter(Context context, ArrayList<ToDoTask> list) {
+//        this.context = context;
+//        this.list = list;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View v = LayoutInflater.from(context).inflate(R.layout.todotask, parent, false);
+//        return new MyViewHolder(v);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+//        ToDoTask toDoTask = list.get(position);
+//        holder.moduleName.setText(toDoTask.getModuleName());
+//        holder.task.setText(toDoTask.getTask());
+//        holder.status.setText(toDoTask.getStatus() + "%");
+//        holder.deadlineDate.setText(toDoTask.getDeadLineDate());
+//        holder.deadlineTime.setText(toDoTask.getDeadLineTime());
+//        holder.plannedDate.setText(toDoTask.getPlannedDate());
+//        holder.plannedTime.setText(toDoTask.getPlannedTime());
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return list.size();
+//    }
+//
+//    public static class MyViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView moduleName, task, status, deadlineDate, deadlineTime, plannedDate, plannedTime;
+//
+//        public MyViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            moduleName = itemView.findViewById(R.id.moduleNameAnnouncements);
+//            task = itemView.findViewById(R.id.toDoTaskAnnouncements);
+//            status = itemView.findViewById(R.id.status);
+//            deadlineDate = itemView.findViewById(R.id.deadlineDateAnnouncements);
+//            deadlineTime = itemView.findViewById(R.id.deadlineTimeAnnouncements);
+//            plannedDate = itemView.findViewById(R.id.plannedDate);
+//            plannedTime = itemView.findViewById(R.id.plannedTime);
+//        }
+//    }
+//}

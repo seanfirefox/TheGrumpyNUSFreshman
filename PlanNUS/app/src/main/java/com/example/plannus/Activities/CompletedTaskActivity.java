ff7d005f24284a48eee4ae6b7e7fcaaeb968f1b0
Tan Li Thai Sean <seanfirefox@gmail.com>
2022-05-29 23:35:13 +0800

    Autosizing for all xml files

--- PlanNUS/app/src/main/java/com/example/plannus/Activities/CompletedTaskActivity.java
+++ PlanNUS/app/src/main/java/com/example/plannus/Activities/CompletedTaskActivity.java
@@ -25,7 +25,7 @@ public class CompletedTaskActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_completed_task);
+        setContentView(R.layout.activity_milestones);
         initVars();
         setUpRecyclerView();
     }

2f671b014e25aa723266142c6b04d6a393f30170
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-06-12 00:09:13 +0800

    Added more heroku HTTPS test

--- Z3Scheduler/TimetableZ3run.py
+++ Z3Scheduler/TimetableZ3run.py
@@ -20,6 +20,13 @@ def show_z3_stuff() :
     timetable = TimeTableSchedulerZ3(scrapper.semesterProcessed, print=True)
     return timetable.optimiseTimetable(to_string=True)
 
+@app.route("/sat")
+def sat_stuff() :
+    mods = ["UTC1114", "GEA1000", "CS2030S", "ENV2102", "UTC1404", "MA2101S", "ST2131", "MA2108S", "CS2040S", "ST2132"]
+    scrapper = Scrapper(mods, "2021-2022", 2)
+    scrapper.scrape()
+    return TimeTableSchedulerZ3(scrapper.semesterProcessed, True).optimiseTimetable(to_string=True)
+
 @app.route("/test")
 def test_one() :
     x = z3.Bool('x')

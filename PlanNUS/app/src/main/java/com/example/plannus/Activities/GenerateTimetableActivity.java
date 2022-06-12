368deab982f422af02c1ebdfe98fc1bc483eb6e8
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-06-12 12:20:30 +0800

    Addons to TimetableGenerator

--- PlanNUS/app/src/main/java/com/example/plannus/Activities/GenerateTimetableActivity.java
+++ PlanNUS/app/src/main/java/com/example/plannus/Activities/GenerateTimetableActivity.java
@@ -34,23 +34,6 @@ public class GenerateTimetableActivity extends AppCompatActivity {
 
         RequestBody requestBody = new FormBody.Builder().add("value", "test succeedd").build();
         Request request = new Request.Builder().url("https://plannus-sat-solver.herokuapp.com/posttest").post(requestBody).build();
-//        okHttpClient.newCall(request).enqueue(new Callback() {
-//            @Override
-//            public void onFailure(Request request, IOException e) {
-//                Log.d("NETWORK_FAIL", "NETWORK FAIL");
-//                TextView textView = findViewById(R.id.textView);
-//                textView.setText("Network Fail");
-//                // Toast.makeText(GenerateTimetableActivity.this, "network not found", Toast.LENGTH_LONG).show();
-//            }
-//
-//            @Override
-//            public void onResponse(Response response) throws IOException {
-//                TextView textView = findViewById(R.id.textView);
-//                String text = response.body().string();
-//                Log.d("RESPONSE_BODY", text);
-//                textView.setText(text);
-//            }
-//        });
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {

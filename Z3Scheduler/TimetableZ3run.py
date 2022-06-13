030c9960046e611c692d0efaa5dd40fbaa9510b4
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-06-14 00:18:15 +0800

    Revert changes for global

--- Z3Scheduler/TimetableZ3run.py
+++ Z3Scheduler/TimetableZ3run.py
@@ -40,8 +40,6 @@ def run() :
 @app.route("/test", methods=['POST'])
 def test_one() :
     global scheduler
-    if (scheduler is not None) :
-        del globals()['scheduler']
     num_mods = int(request.form['numMods'])
     mods = []
     for i in range(num_mods) :
@@ -56,7 +54,6 @@ def test_one() :
 
 @app.route("/alt_soln", methods=["POST"])
 def alt_soln() :
-    global scheduler
     if (scheduler is None) :
         return "Nothing generated yet! No other solution offered!"
     return scheduler.another_solution(to_string=True)
@@ -77,39 +74,3 @@ def post_from_android() :
 
 if __name__ == "__main__" :
     app.run()
-
-'''
-OPTIONS = [
-
-]
-numMods = len(sys.argv) - 1
-
-print("You have entered " + str(numMods) + "modules\n") 
-AY = input("Enter Academic Year in the form 2021-2022\n")
-SEM = int(input("Input semester number\n"))
-modules = [sys.argv[i] for i in range(1, numMods + 1)]
-
-scrapper = Scrapper(modules, AY, SEM)
-scrapper.scrape()
-
-timetable = TimeTableSchedulerZ3(scrapper.semesterProcessed, print=True)
-timetable.optimiseTimetable()
-
-if (timetable.last_solution_status() == unsat) :
-    print("Terminating . . . \n")
-else :
-    happy = False
-    print("Are You Happy with this timetable? \n")
-    happy = input("Yes or No only \n") == "Yes"
-    if (happy != True and happy != False) :
-        print("Wrong input! \n")
-    while(happy != True) :
-        print("Regenerating timetable... \n")
-        timetable.another_solution()
-        print("Are You Happy with this timetable? \n")
-        happy = input("Yes or No only \n") == "Yes"
-        if (happy != True and happy != False) :
-            print("Wrong input! ")
-    if (happy) :
-        print("Saving Timetable...\n")
-'''

2367760797c1b3b5a587de3f4a56c280b6bf8f7b
Tan Li Thai Sean <seanfirefox@gmail.com>
2022-06-21 11:57:14 +0800

    edit database uri local

--- Z3Scheduler/Z3Schedule/__init__.py
+++ Z3Scheduler/Z3Schedule/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
+            'sqlite:///' + os.path.join(app.instance_path, 'Z3Schedule.sqlite'),
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 

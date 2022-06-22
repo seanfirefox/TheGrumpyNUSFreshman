07b86894998b140e58ce95b4faf19f7082dfd1e6
Tan Li Thai Sean <seanfirefox@gmail.com>
2022-06-22 12:03:21 +0800

    Fragments with bottom Navigation Bar working

--- PlanNUS/app/src/main/java/com/example/plannus/Activities/CalendarActivity.java
+++ PlanNUS/app/src/main/java/com/example/plannus/Activities/CalendarActivity.java
@@ -1,22 +1,28 @@
 package com.example.plannus.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.example.plannus.Fragments.FridayFragment;
 import com.example.plannus.Fragments.MondayFragment;
+import com.example.plannus.Fragments.ThursdayFragment;
 import com.example.plannus.Fragments.TuesdayFragment;
+import com.example.plannus.Fragments.WednesdayFragment;
 import com.example.plannus.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class CalendarActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button monButton, tueButton;
+public class CalendarActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +32,31 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.mondayButton) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.monButton) {
+            Log.d("mon", "selected");
             changeFragment(MondayFragment.class);
-        } else if (v.getId() == R.id.tuesdayButton) {
+        } else if (item.getItemId() == R.id.tueButton) {
+            Log.d("tue", "selected");
             changeFragment(TuesdayFragment.class);
+        } else if (item.getItemId() == R.id.wedButton) {
+            Log.d("wed", "selected");
+            changeFragment(WednesdayFragment.class);
+        } else if (item.getItemId() == R.id.thursButton) {
+            changeFragment(ThursdayFragment.class);
+        } else if (item.getItemId() == R.id.friButton) {
+            changeFragment(FridayFragment.class);
+        } else {
+            Log.d("fail", "nothing");
         }
+
+        return true;
     }
 
+
     public void initVars() {
-        monButton = findViewById(R.id.mondayButton);
-        monButton.setOnClickListener(this);
-        tueButton = findViewById(R.id.tuesdayButton);
-        tueButton.setOnClickListener(this);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        bottomNavigationView.setOnItemSelectedListener(this);
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.setReorderingAllowed(true);
@@ -47,8 +65,8 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
     public void changeFragment(Class<? extends Fragment> fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .setReorderingAllowed(true)
                 .replace(R.id.fragmentContainerView, fragment, null)
                 .commit();
     }
+
 }

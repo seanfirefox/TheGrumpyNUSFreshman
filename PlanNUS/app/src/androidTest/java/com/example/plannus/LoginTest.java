ad6449d303b14296c3cddf082b9419eae8803bb1
Tan Li Thai Sean <seanfirefox@gmail.com>
2022-06-30 21:47:16 +0800

    UI first test on Login working

--- PlanNUS/app/src/androidTest/java/com/example/plannus/LoginTest.java
+++ PlanNUS/app/src/androidTest/java/com/example/plannus/LoginTest.java
@@ -1,26 +1,36 @@
 package com.example.plannus;
 
+import androidx.test.platform.app.InstrumentationRegistry;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.view.View;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import com.example.plannus.Activities.ContentMainActivity;
 import com.example.plannus.Activities.MainActivity;
 
 /**
@@ -38,15 +48,21 @@ public class LoginTest {
     private String email = "admin2@gmail.com";
     private String password = "1234567";
 
+    @Before
+    public void setUp() throws Exception{
+        Intents.init();
+    }
+
     @Test
     public void clickLoginButton() {
-        // Context of the app under test.
-//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-//        assertEquals("com.example.plannus", appContext.getPackageName());
         onView(withId(R.id.emailAddress)).perform(ViewActions.typeText(email));
         onView(withId(R.id.passWord)).perform(ViewActions.typeText(password));
         onView(withId(R.id.loginButton)).perform(ViewActions.scrollTo(), ViewActions.click());
+        intending(hasComponent(ContentMainActivity.class.getName()));
+    }
 
-        Espresso.onView(withId(R.id.contentMain)).check(matches(withText("Welcome Back !")));
+    @After
+    public void tearDown() throws Exception{
+        Intents.release();
     }
 }

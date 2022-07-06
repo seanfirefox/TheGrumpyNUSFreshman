6a1f08c3b59a7493c5f7167371e80945a20ea32a
Tan Li Thai Sean <seanfirefox@gmail.com>
2022-07-06 12:34:49 +0800

    MetricsConverter test done with Mockito, TimeFormatter Test done

--- PlanNUS/app/src/test/java/com/example/plannus/MetricsConverterUtilTest.java
+++ PlanNUS/app/src/test/java/com/example/plannus/MetricsConverterUtilTest.java
@@ -11,23 +11,32 @@ import com.example.plannus.utils.MetricsConverter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.SuppressStaticInitializationFor;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(ClassToMock.class)
-public class MetricsConverterTest {
+@PrepareForTest(Resources.class)
+@PowerMockIgnore({"android.content.res.Resources"})
+@SuppressStaticInitializationFor({"Resources"})
+public class MetricsConverterUtilTest {
 
     @Test
     public void testForDaysandHours() {
-        PowerMockito.mockStatic(Resources.class,RETURNS_MOCKS);
-        Resources res = mock(Resources.getSystem().getClass());
+        PowerMockito.mockStatic(Resources.class, RETURNS_MOCKS);
+        Resources res = mock(Resources.class);
+        Mockito.when(Resources.getSystem()).thenReturn(res);
         DisplayMetrics metrics = mock(DisplayMetrics.class);
-        metrics.widthPixels = 300;
-        PowerMockito.when(res.getDisplayMetrics()).thenReturn(metrics);
+        metrics.densityDpi = 440;
+        Mockito.when(res.getDisplayMetrics()).thenReturn(metrics);
+
         float dp = 20f;
         int pixel = MetricsConverter.convertDpToPixel(dp);
-        assertEquals(1, pixel);
+        assertEquals(55, pixel);
     }
+
 }
+

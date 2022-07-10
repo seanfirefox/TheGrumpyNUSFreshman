2e52d94eba9d5599af8ca374c8823fe8329d037c
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-07-10 17:18:05 +0800

    Milestones Cardview to include trophies (generated at random)

--- PlanNUS/app/src/main/java/com/example/plannus/Adaptors/CompletedTaskAdapter.java
+++ PlanNUS/app/src/main/java/com/example/plannus/Adaptors/CompletedTaskAdapter.java
@@ -3,6 +3,7 @@ package com.example.plannus.Adaptors;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,21 +21,28 @@ public class CompletedTaskAdapter extends FirestoreRecyclerAdapter<ToDoTask, Com
         super(options);
     }
 
+    private static double randDouble = Math.random();
+
     @Override
     protected void onBindViewHolder(@NonNull CompletedTaskAdapter.FinishedTaskHolder holder, int position, @NonNull ToDoTask model) {
-        String moduleName, task, status, deadlineDate, deadlineTime, plannedDate, plannedTime;
+        String moduleName, task, deadlineDate, deadlineTime, plannedDate, plannedTime;
 
         moduleName = model.getModuleName();
         task = model.getTask();
-        status = model.getStatus();
         deadlineDate = DateFormatter.numberToDate(model.getDeadLineDateTime().substring(0, 8));
         deadlineTime = TimeFormatter.numberToTime(model.getDeadLineDateTime().substring(8, 12));
         plannedDate = DateFormatter.numberToDate(model.getPlannedDateTime().substring(0, 8));
         plannedTime = TimeFormatter.numberToTime(model.getPlannedDateTime().substring(8, 12));
 
+        double luckyNumber = Math.random();
+
         holder.taskName.setText(task);
+        holder.imageReward.setImageResource(
+                luckyNumber < 0.333 ? R.drawable.ic_ribbon_sticker_svgrepo_com :
+                        luckyNumber < 0.666 ? R.drawable.ic_medal_svgrepo_com :
+                                R.drawable.ic_trophy_svgrepo_com
+        );
         holder.tagName.setText(moduleName);
-        holder.status.setText(status + "%");
         holder.dueDate.setText(deadlineDate);
         holder.dueTime.setText(deadlineTime);
         holder.plannedDate.setText(plannedDate);
@@ -51,13 +59,14 @@ public class CompletedTaskAdapter extends FirestoreRecyclerAdapter<ToDoTask, Com
 
     public static class FinishedTaskHolder extends RecyclerView.ViewHolder {
 
-        private TextView taskName, tagName, status, dueDate, dueTime, plannedDate, plannedTime;
+        private TextView taskName, tagName, dueDate, dueTime, plannedDate, plannedTime;
+        private ImageView imageReward;
 
         public FinishedTaskHolder(View itemView) {
             super(itemView);
             taskName = itemView.findViewById(R.id.toDoTaskNameCompleted);
             tagName = itemView.findViewById(R.id.tagNameCompleted);
-            status = itemView.findViewById(R.id.statusIntCompleted);
+            imageReward = itemView.findViewById(R.id.completedReward);
             dueDate = itemView.findViewById(R.id.deadlineDateCompleted);
             dueTime = itemView.findViewById(R.id.deadlineTimeCompleted);
             plannedDate = itemView.findViewById(R.id.PlannedDateCompleted);

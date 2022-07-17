547c1cc8c31c0b217d4720fec209aefd162abce5
Tan Kai Min Russell <russelltanreg38@gmail.com>
2022-07-17 18:08:50 +0800

    Fix unscrollable (for some) Recyclerviews

--- PlanNUS/app/src/main/java/com/example/plannus/Fragments/ChildFragments/ChildClassFragment/TuesdayClassFragment.java
+++ PlanNUS/app/src/main/java/com/example/plannus/Fragments/ChildFragments/ChildClassFragment/TuesdayClassFragment.java
@@ -42,6 +42,7 @@ public class TuesdayClassFragment extends Fragment {
         sessionManager = SessionManager.get();
         userID = sessionManager.getUserID();
         recyclerView = view.findViewById(R.id.tuesdayRecyclerVIew);
+        recyclerView.setNestedScrollingEnabled(false);
     }
 
     private void setupRecyclerView(View view) {
